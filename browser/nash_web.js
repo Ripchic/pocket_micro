9d1725c442bb572d938e5e2b62c87b6292d8ff15
Ripchic <theripper25@mail.ru>
2024-02-12 22:57:02 +0300

    table visualisation

--- browser/nash_web.js
+++ browser/nash_web.js
@@ -33,7 +33,7 @@ class Nash_browser_game {
                     cell.innerHTML = `Strategy ${i + 1}`;
                 } else {
                     const inputId = `cell-${i}-${j - 1}`;
-                    cell.innerHTML = `<input type='text' id='${inputId}' class='strategy-cell' />`; // Use input for strategy cells
+                    cell.innerHTML = `<input type='text' id='${inputId}'/>`;
                 }
             }
         }
@@ -59,7 +59,6 @@ class Nash_browser_game {
 
     generate_results_table() {
         console.log('generate_results_table');
-        console.log(this.nash_game.x);
         this.tableContainer.innerHTML = '';
 
         const table = document.createElement('table');
@@ -68,6 +67,7 @@ class Nash_browser_game {
 
         let row = thead.insertRow();
         let cell = row.insertCell();
+        cell.innerHTML = 'Player 1 \\ Player 2';
         for (let i = 0; i < this.nash_game.y; i++) {
             cell = row.insertCell();
             cell.colSpan = 2; // Span two columns
@@ -84,14 +84,84 @@ class Nash_browser_game {
                 const values = this.read_cell(i, j);
                 let cell1 = row.insertCell();
                 cell1.innerHTML = values[0];
+                cell1.id = `cell-${i}-${j}-0`;
 
                 let cell2 = row.insertCell();
                 cell2.innerHTML = values[1];
+                cell2.id = `cell-${i}-${j}-1`;
             }
         }
 
         this.tableContainer.appendChild(table);
     }
+
+    async find_and_mark_nash_equilibrium() {
+        console.log('find_and_mark_nash_equilibrium');
+
+        for (let i = 0; i < this.nash_game.x; i++) {
+            let max_values = this.read_cell(i, 0);
+            let max_ids = [0];
+            for (let j = 0; j < this.nash_game.y; j++) {
+                const values = this.read_cell(i, j);
+                if (values[1] > max_values[1]) {
+                    max_values[1] = values[1];
+                    max_ids = [j];
+                } else if (values[1] === max_values[1]) {
+                    max_ids.push(j);
+                }
+                const cell = document.getElementById(`cell-${i}-${j}-1`);
+                cell.style.backgroundColor = 'grey';
+                await this.sleep(300);
+            }
+            for (let j of max_ids) {
+                const cell = document.getElementById(`cell-${i}-${j}-1`);
+                cell.style.backgroundColor = 'green';
+                this.nash_game.optimal_choice[i][j][1] = 1;
+                await this.sleep(500);
+            }
+            for (let j = 0; j < this.nash_game.y; j++) {
+                if (!max_ids.includes(j)) {
+                    const cell = document.getElementById(`cell-${i}-${j}-1`);
+                    cell.style.backgroundColor = 'white';
+                }
+            }
+        }
+
+        for (let i = 0; i < this.nash_game.y; i++) {
+            let max_values = this.read_cell(i, 0);
+            let max_ids = [0];
+            for (let j = 0; j < this.nash_game.x; j++) {
+                const values = this.read_cell(i, j);
+                if (values[0] > max_values[0]) {
+                    max_values[0] = values[0];
+                    max_ids = [j];
+                } else if (values[0] === max_values[0]) {
+                    max_ids.push(j);
+                }
+                const cell = document.getElementById(`cell-${j}-${i}-0`);
+                cell.style.backgroundColor = 'grey';
+                await this.sleep(300);
+            }
+
+            for (let j of max_ids) {
+                const cell = document.getElementById(`cell-${j}-${i}-0`);
+                cell.style.backgroundColor = 'green';
+                this.nash_game.optimal_choice[i][j][1] = 1;
+                await this.sleep(500);
+            }
+            for (let j = 0; j < this.nash_game.x; j++) {
+                if (!max_ids.includes(j)) {
+                    const cell = document.getElementById(`cell-${j}-${i}-0`);
+                    cell.style.backgroundColor = 'white';
+                }
+            }
+        }
+    }
+
+    sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
+
 }
 
 let game = NaN;
@@ -106,4 +176,5 @@ document.getElementById("generateTable").addEventListener("click", () => {
 
 document.getElementById("runNash").addEventListener("click", () => {
     game.generate_results_table();
+    game.find_and_mark_nash_equilibrium();
 });
